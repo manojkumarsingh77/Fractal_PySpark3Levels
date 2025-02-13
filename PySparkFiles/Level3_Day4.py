c6d8ac0644f2883ac2150ffda7c64b77e0afee55
MANOJ KUMAR <manojsc99@gmail.com>
2025-02-13 10:46:30 +0530

    Update Level3_Day4.py

--- PySparkFiles/Level3_Day4.py
+++ PySparkFiles/Level3_Day4.py
@@ -1,3 +1,8 @@
 # Generating a sample DataFrame
 data = [(1, "Alice", 5000), (2, "Bob", 7000), (3, "Charlie", 9000)]
 columns = ["CustomerID", "Name", "Balance"]
+
+
+
+data = [(1, "Alice", 5000), (2, None, 7000), (3, "Charlie", None)]
+columns = ["CustomerID", "Name", "Balance"]
