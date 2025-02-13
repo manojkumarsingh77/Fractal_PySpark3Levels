6aea7ec6330117d763b8da25b177ca1f8b8bf9f0
MANOJ KUMAR <manojsc99@gmail.com>
2025-02-13 10:52:28 +0530

    Update Level3_Day4.py

--- PySparkFiles/Level3_Day4.py
+++ PySparkFiles/Level3_Day4.py
@@ -6,3 +6,6 @@ columns = ["CustomerID", "Name", "Balance"]
 
 data = [(1, "Alice", 5000), (2, None, 7000), (3, "Charlie", None)]
 columns = ["CustomerID", "Name", "Balance"]
+
+
+df.select([col(c).isNull().alias(c) for c in df.columns]).show()
